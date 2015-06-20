c53995cfbeeb822d511a0d27b91807086ef53849
Braiden Vasco <braiden-vasco@mailtor.net>
2015-06-21 01:53:39 +0500

    tmp

--- lib/lita/handlers/random.rb
+++ lib/lita/handlers/random.rb
@@ -10,16 +10,16 @@ module Lita
     # Generator of random numbers and strings for the Lita chat bot.
     #
     class Random < Handler
-      route /^rand(om)?$/i, command: true do |response|
+      route(/^rand(om)?$/i, command: true) do |response|
         response.reply(::Random.rand.to_s)
       end
 
-      route /^rand(om)?\s+(\d+)?$/i, command: true do |response|
+      route(/^rand(om)?\s+(\d+)?$/i, command: true) do |response|
         to = response.matches[0][1].to_i
         response.reply(::Random.rand(to).to_s)
       end
 
-      route /^rand(om)?\s+(\d+)\s+(\d+)?$/i, command: true do |response|
+      route(/^rand(om)?\s+(\d+)\s+(\d+)?$/i, command: true) do |response|
         from = response.matches[0][1].to_i
         to = response.matches[0][2].to_i
         response.reply(::Random.rand(from...to).to_s)
