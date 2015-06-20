c7cabcdc90b10d1768c94e651a60264161bf1352
Braiden Vasco <braiden-vasco@mailtor.net>
2015-06-20 22:45:16 +0500

    Initialize gem $ bundle gem .

---
+++ lib/lita/random.rb
@@ -0,0 +1,7 @@
+require "lita/random/version"
+
+module Lita
+  module Random
+    # Your code goes here...
+  end
+end
---
+++ lib/lita/random/version.rb
@@ -0,0 +1,5 @@
+module Lita
+  module Random
+    VERSION = "0.1.0"
+  end
+end
