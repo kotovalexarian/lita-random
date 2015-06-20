e9b7c618026ab5bdc6f2427f764002938def710a
Braiden Vasco <braiden-vasco@mailtor.net>
2015-06-21 01:21:19 +0500

    Add documentation

--- lib/lita/handlers/random.rb
+++ lib/lita/handlers/random.rb
@@ -1,5 +1,14 @@
+##
+# Lita module.
+#
 module Lita
+  ##
+  # Lita handlers module.
+  #
   module Handlers
+    ##
+    # Generator of random numbers and strings for the Lita chat bot.
+    #
     class Random < Handler
     end
 
