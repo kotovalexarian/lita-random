edfb0997adbcaaec55960480397dceeed743bdf6
Braiden Vasco <braiden-vasco@mailtor.net>
2015-06-21 11:29:29 +0500

    Add specs

--- spec/lita/handlers/random_spec.rb
+++ spec/lita/handlers/random_spec.rb
@@ -136,4 +136,64 @@ describe Lita::Handlers::Random, lita_handler: true do
       expect(value).to be < 3.4
     end
   end
+
+  it { is_expected.to route_command('random  base64').to :route_random_base64 }
+  it { is_expected.to route_command('rand  base64').to :route_random_base64 }
+  it { is_expected.to route_command('rAnDoM bAsE64').to :route_random_base64 }
+  it { is_expected.to route_command('RaNd   BaSe64').to :route_random_base64 }
+
+  it do
+    is_expected.to route_command('random  base64 0').to :route_random_base64_n
+  end
+
+  it do
+    is_expected.to route_command('rand  base64  523').to :route_random_base64_n
+  end
+
+  it do
+    is_expected.to route_command('rAnDoM bAsE64 5').to :route_random_base64_n
+  end
+
+  it do
+    is_expected.to route_command('RaNd BaSe64   4925').to :route_random_base64_n
+  end
+
+  describe '/random base64' do
+    it 'replies in correct format' do
+      send_command('random base64')
+      expect(replies.last).to match %r{\A[A-Za-z0-9+/]{22}==\z}
+    end
+  end
+
+  describe '/random base64 <n>' do
+    it 'replies in correct format' do
+      send_command('random base64 0')
+      expect(replies.last).to eq ''
+    end
+
+    it 'replies in correct format' do
+      send_command('random base64 1')
+      expect(replies.last).to match %r{\A[A-Za-z0-9+/]{2}==\z}
+    end
+
+    it 'replies in correct format' do
+      send_command('random base64 2')
+      expect(replies.last).to match %r{\A[A-Za-z0-9+/]{3}=\z}
+    end
+
+    it 'replies in correct format' do
+      send_command('random base64 3')
+      expect(replies.last).to match %r{\A[A-Za-z0-9+/]{4}\z}
+    end
+
+    it 'replies in correct format' do
+      send_command('random base64 34')
+      expect(replies.last).to match %r{\A[A-Za-z0-9+/]{46}==\z}
+    end
+
+    it 'replies in correct format' do
+      send_command('random base64 35')
+      expect(replies.last).to match %r{\A[A-Za-z0-9+/]{47}=\z}
+    end
+  end
 end
