5d1d0f3c2d868245f1ce3b8de2d0aab4f84e9c75
Braiden Vasco <braiden-vasco@mailtor.net>
2015-06-24 14:54:30 +0500

    Add specs for command "sample <n>"

--- spec/lita/handlers/random_spec.rb
+++ spec/lita/handlers/random_spec.rb
@@ -638,4 +638,56 @@ describe Lita::Handlers::Random, lita_handler: true do
         .to include replies.last
     end
   end
+
+  #############################
+  #  /sample <n> <array, ...>
+  #############################
+
+  it { is_expected.to route_command('SaMpLe 1  a').to :route_sample }
+  it { is_expected.to route_command('sAMpLE  1703 A,   b').to :route_sample }
+  it { is_expected.to route_command('SamPle   2 a, B,  c').to :route_sample }
+  it { is_expected.to route_command('saMPle 98 1').to :route_sample }
+  it { is_expected.to route_command('SAmpLE  746  1, b').to :route_sample }
+  it { is_expected.to route_command('saMplE 9 1,   2, 3').to :route_sample }
+  it { is_expected.to route_command('samPLe  3 1,b,C').to :route_sample }
+  it { is_expected.to route_command('sAmple 147  1,2,3').to :route_sample }
+
+  describe '/sample <n> <array, ...>' do
+    it 'replies in correct format' do
+      send_command 'sample  0  a'
+      expect(replies.last).to eq ''
+    end
+
+    it 'replies in correct format' do
+      send_command 'sample  1  foo'
+      expect(replies.last).to eq 'foo'
+    end
+
+    it 'replies in correct format' do
+      send_command 'sample  2  a'
+      expect(replies.last).to eq 'a'
+    end
+
+    it 'replies in correct format' do
+      send_command 'sample  0  foo,bar'
+      expect(replies.last).to eq ''
+    end
+
+    it 'replies in correct format' do
+      send_command 'sample  1  foo,bar'
+      expect(%w(foo bar)).to include replies.last
+    end
+
+    it 'replies in correct format' do
+      send_command 'sample  2  foo  ,  bar'
+      expect(replies.last.split(',').map(&:lstrip))
+        .to match_array %w(foo bar)
+    end
+
+    it 'replies in correct format' do
+      send_command 'sample  3  a,b'
+      expect(replies.last.split(',').map(&:lstrip))
+        .to match_array %w(a b)
+    end
+  end
 end
