ec0060d9d9a34be20ec6fb1c3e30ea57177bd639
Braiden Vasco <braiden-vasco@mailtor.net>
2015-06-24 15:43:30 +0500

    Add specs for command "random case <s>"

--- spec/lita/handlers/random_spec.rb
+++ spec/lita/handlers/random_spec.rb
@@ -137,6 +137,66 @@ describe Lita::Handlers::Random, lita_handler: true do
     end
   end
 
+  #####################
+  #  /random case <s>
+  #####################
+
+  it { is_expected.to route_command('random  case').to :route_random_case }
+  it { is_expected.to route_command('rand  case').to :route_random_case }
+  it { is_expected.to route_command('rAnDoM cAsE').to :route_random_case }
+  it { is_expected.to route_command('RaNd   CaSe').to :route_random_case }
+
+  it { is_expected.to route_command('randomcase').to :route_random_case }
+  it { is_expected.to route_command('randcase').to :route_random_case }
+  it { is_expected.to route_command('rAnDoMcAsE').to :route_random_case }
+  it { is_expected.to route_command('RaNdCaSe').to :route_random_case }
+
+  describe '/random case <s>' do
+    it 'replies in correct format' do
+      send_command 'random case'
+      expect(replies.last).to eq ''
+    end
+
+    it 'replies in correct format' do
+      send_command 'RaNd CaSe'
+      expect(replies.last).to eq ''
+    end
+
+    it 'replies in correct format' do
+      send_command 'RaNdOmCaSe'
+      expect(replies.last).to eq ''
+    end
+
+    it 'replies in correct format' do
+      send_command 'rAnDcAsE'
+      expect(replies.last).to eq ''
+    end
+
+    it 'replies in correct format' do
+      send_command 'random case  1'
+      expect(replies.last).to eq '1'
+    end
+
+    it 'replies in correct format' do
+      send_command 'RaNd CaSe  foo'
+      expect(replies.last).to match(/\Afoo\z/i)
+    end
+
+    it 'replies in correct format' do
+      send_command 'RaNdOmCaSe foo  bar'
+      expect(replies.last).to match(/\Afoo  bar\z/i)
+    end
+
+    it 'replies in correct format' do
+      send_command 'rAnDcAsE  12345'
+      expect(replies.last).to eq '12345'
+    end
+  end
+
+  ##########################
+  #  /random base64 <n=16>
+  ##########################
+
   it { is_expected.to route_command('random  base64').to :route_random_base64 }
   it { is_expected.to route_command('rand  base64').to :route_random_base64 }
   it { is_expected.to route_command('rAnDoM bAsE64').to :route_random_base64 }
@@ -233,6 +293,10 @@ describe Lita::Handlers::Random, lita_handler: true do
       expect(replies.last).to match %r{\A[A-Za-z0-9+/]{47}=\z}
     end
   end
+
+  #######################
+  #  /random hex <n=16>
+  #######################
 
   it { is_expected.to route_command('random  Hx').to :route_random_hex }
   it { is_expected.to route_command('rand  hex').to :route_random_hex }
