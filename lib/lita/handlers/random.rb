78566b453cac146e5e1ba60f2f8ae8e57829896c
Braiden Vasco <braiden-vasco@mailtor.net>
2015-06-25 15:14:35 +0500

    Move implementation of command "random" to single route

--- lib/lita/handlers/random.rb
+++ lib/lita/handlers/random.rb
@@ -53,39 +53,20 @@ module Lita
           'choose `n` random elements from `array`',
       }
 
-      route(/^rand(om)?$/i, :route_random, command: true, help: HELP)
+      route(/^rand(om)?((\s+(?<from>\d+(\.\d+)?))?\s+(?<to>\d+(\.\d+)?))?$/i,
+            :route_random, command: true, help: HELP)
       def route_random(response)
-        response.reply(::Random.rand.to_s)
-      end
-
-      route(/^rand(om)?\s+(\d+)?$/i, :route_random_to, command: true)
-      def route_random_to(response)
-        to = response.matches[0][1].to_i
-        response.reply(::Random.rand(to).to_s)
-      end
-
-      route(/^rand(om)?\s+(\d+\.\d+)?$/i, :route_random_float_to, command: true)
-      def route_random_float_to(response)
-        to = response.matches[0][1].to_f
-        response.reply(::Random.rand(to).to_s)
-      end
-
-      route(/^rand(om)?\s+(\d+)\s+(\d+)?$/i,
-            :route_random_from_to, command: true)
-      def route_random_from_to(response)
-        from = response.matches[0][1].to_i
-        to = response.matches[0][2].to_i
-        response.reply(::Random.rand(from...to).to_s)
-      end
-
-      route(/^rand(om)?\s+(?<from>\d+\.\d+)\s+(?<to>\d+)?$/i,
-            :route_random_float_from_to, command: true)
-      route(/^rand(om)?\s+(?<from>\d+(\.\d+)?)\s+(?<to>\d+\.\d+)?$/i,
-            :route_random_float_from_to, command: true)
-      def route_random_float_from_to(response)
         matches = response.matches[0]
-        from = matches[0].to_f
-        to = matches[1].to_f
+
+        to_numeric = lambda do |index|
+          if matches[index]
+            matches[index].send(matches[index] =~ /\./ ? :to_f : :to_i)
+          end
+        end
+
+        from = to_numeric.(0) || 0
+        to   = to_numeric.(1) || 1.0
+
         response.reply(::Random.rand(from...to).to_s)
       end
 
