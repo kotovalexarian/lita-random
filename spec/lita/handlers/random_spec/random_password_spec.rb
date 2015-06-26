0b42d174b8bc2c7b7da8aa7b5156953fcce1aa5a
Braiden Vasco <braiden-vasco@mailtor.net>
2015-06-26 10:03:20 +0500

    Add specs for command "random password"

--- spec/lita/handlers/random_spec/random_password_spec.rb
+++ spec/lita/handlers/random_spec/random_password_spec.rb
@@ -10,20 +10,26 @@ describe Lita::Handlers::Random, lita_handler: true do
   it { is_expected.to route_command('rANdpASs').to :route_random_pass }
 
   it do
-    is_expected.to route_command('RaNdOm pAsSwOrD  0').to :route_random_pass_n
+    is_expected.to route_command('RaNdOm pAsSwOrD  0').to :route_random_pass
   end
 
-  it { is_expected.to route_command('rAnD PaSsWoRd 1').to :route_random_pass_n }
-  it { is_expected.to route_command('RanDOm PasS 123').to :route_random_pass_n }
-  it { is_expected.to route_command('RaNd pAsS  4567').to :route_random_pass_n }
+  it { is_expected.to route_command('rAnD PaSsWoRd 1').to :route_random_pass }
+  it { is_expected.to route_command('RanDOm PasS 123').to :route_random_pass }
+  it { is_expected.to route_command('RaNd pAsS  4567').to :route_random_pass }
 
   it do
-    is_expected.to route_command('RanDOmpASswORd  890').to :route_random_pass_n
+    is_expected.to route_command('RanDOmpASswORd  890').to :route_random_pass
   end
 
-  it { is_expected.to route_command('rAndpAsSWord 09').to :route_random_pass_n }
-  it { is_expected.to route_command('RanDomPass  876').to :route_random_pass_n }
-  it { is_expected.to route_command('rANdpASs   5432').to :route_random_pass_n }
+  it { is_expected.to route_command('rAndpAsSWord 09').to :route_random_pass }
+  it { is_expected.to route_command('RanDomPass  876').to :route_random_pass }
+  it { is_expected.to route_command('rANdpASs   5432').to :route_random_pass }
+
+  it { is_expected.to route_command('rANdpASs -l 5').to :route_random_pass }
+
+  it do
+    is_expected.to route_command('rANdpASs --length 5').to :route_random_pass
+  end
 
   describe '/random password' do
     it 'replies in correct format' do
@@ -50,6 +56,16 @@ describe Lita::Handlers::Random, lita_handler: true do
 
     it 'replies in correct format' do
       send_command('rAnDpAsS 32')
+      expect(replies.last).to match(/\A[\w\d]{32}\z/)
+    end
+
+    it 'replies in correct format' do
+      send_command('rAnDpAsS -l 32')
+      expect(replies.last).to match(/\A[\w\d]{32}\z/)
+    end
+
+    it 'replies in correct format' do
+      send_command('rAnDpAsS --length 32')
       expect(replies.last).to match(/\A[\w\d]{32}\z/)
     end
   end
